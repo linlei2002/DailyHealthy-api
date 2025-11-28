1c2824dca1501009b9785d23c42e930f3d66475e
linlei2002 <3119549411@qq.com>
2025-11-28 08:49:41 +0800

    feat:项目初始化

---
+++ src/main/java/top/llin/dailyhealthy/DailyHealthyApplication.java
@@ -0,0 +1,13 @@
+package top.llin.dailyhealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHealthyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyHealthyApplication.class, args);
+    }
+
+}
